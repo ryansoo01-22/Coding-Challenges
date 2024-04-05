8294b64340b3c3acc4ac9469380ded3b4bb102d8
soorMSWE <soor@uci.edu>
2024-04-05 13:29:11 -0700

    work on step 3

--- Compression Tool/Compression Tool.py
+++ Compression Tool/Compression Tool.py
@@ -1,6 +1,6 @@
 import collections
 
-def stepOne():
+def step_one():
     with open('135-0.txt', 'r', encoding='utf-8') as f:
         text = f.read()
         letter_freqs = {}
@@ -8,7 +8,10 @@ def stepOne():
             if c.isalnum():
                 letter_freqs[c] = 1 + letter_freqs.get(c, 0)
         sorted_freqs = sorted(letter_freqs.items(), key=lambda x:x[1])
-        return sorted_freqs
+        list_freqs = []
+        for i in sorted_freqs:
+            list_freqs.append(Node(i[0], i[1]))
+        return list_freqs
 
 class Node:
     data = ""
@@ -40,20 +43,37 @@ class HuffmanTree:
     
     def __init__(self, root) -> None:
         self.root = root
-       
+
+def step_two(sorted_nodes):
+    while len(sorted_nodes) > 1:
+        first, second = sorted_nodes.pop(0), sorted_nodes.pop(0)
+        root_weight = first.weight + second.weight
+        root_node = Node(None, root_weight)
+        root_node.left = first
+        first.parent = root_node
+        root_node.right = second
+        second.parent = root_node
+        sorted_nodes.append(root_node)
+        sorted_nodes.sort()
+    huffman_tree = HuffmanTree(sorted_nodes.pop())
+    return huffman_tree
+
 def make_tree_test(sorted_nodes):
     while len(sorted_nodes) > 1:
         first, second = sorted_nodes.pop(0), sorted_nodes.pop(0)
         root_weight = first.weight + second.weight
         root_node = Node(None, root_weight)
         root_node.left = first
+        first.parent = root_node
         root_node.right = second
+        second.parent = root_node
         sorted_nodes.append(root_node)
         sorted_nodes.sort()
-    print("test")
+    huffman_tree = HuffmanTree(sorted_nodes.pop())
+    return huffman_tree
 
 def step_two_testing():
-    '''using this example to make the tree https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/Huffman.html'''
+    #using this example to make the tree https://opendsa-server.cs.vt.edu/ODSA/Books/CS3/html/Huffman.html
     test_list = []
     test_list.append(Node("C", 32))
     test_list.append(Node("D", 42))
@@ -64,8 +84,35 @@ def step_two_testing():
     test_list.append(Node("U", 37))
     test_list.append(Node("Z", 2))
     test_list.sort()
-    make_tree_test(test_list)
+    return make_tree_test(test_list)
 
-step_two_testing()
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+
+        if root.data:
+            print(root.data)
+
+        inorder_traversal(root.right)
+
+def step_three_test(tree):
+    inorder_traversal(tree.root)
+    #NEED TO MAKE LOOKUP TABLE
+    
+if __name__ == "__main__":
+    step_two_tree = step_two_testing()
+    step_three_test(step_two_tree)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
